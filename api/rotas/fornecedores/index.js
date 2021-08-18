80c6174a093812478aa55b918c63158684ba05dd
Joadson Silva <joadson83@hotmail.com>
2021-08-18 19:47:29 -0300

    persistent data

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,9 +1,17 @@
 const roteador = require('express').Router()
 const TableFornecedor = require('./tableFornecedor')
+const Fornecedor = require('./Fornecedor')
 
-roteador.use('/', async (req, res) => {
+roteador.get('/', async (req, res) => {
     const resultados = await TableFornecedor.listar()
     res.send(JSON.stringify(resultados))
+})
+
+roteador.post('/', async (req, res) => {
+    const dadosRecebidos = req.body
+    const fornecedor = new Fornecedor(dadosRecebidos)
+    await fornecedor.criar()
+    res.send(JSON.stringify(fornecedor))
 })
 
 module.exports = roteador
--- api/rotas/fornecedores/tableFornecedor.js
+++ api/rotas/fornecedores/tableFornecedor.js
@@ -3,5 +3,8 @@ const Model = require("./modelTabFornecedor")
 module.exports = {
     listar () {
         return Model.findAll()
+    },
+    inserir(fornecedor){
+        return Model.create(fornecedor)
     }
 }
